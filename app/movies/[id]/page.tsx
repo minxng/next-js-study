ab70e19bc3d3d6448b870e81a18c14d4d936a34a
minxng <minxng12@gmail.com>
2025-03-12 00:32:00 +0900

    param promise 사용

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -2,20 +2,20 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
 
-interface IParams {
-  id: string;
-}
+type Params = Promise<{ id: string }>;
 
-export async function generateMetadata({ params }: { params: IParams }) {
-  const { id } = params;
+export async function generateMetadata(props: { params: Params }) {
+  const params = await props.params;
+  const id = params.id;
   const movie = await getMovie(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetail({ params }: { params: IParams }) {
-  const { id } = params;
+export default async function MovieDetail(props: { params: Params }) {
+  const params = await props.params;
+  const id = params.id;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info..</h1>}>
