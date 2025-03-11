51acb5756df0e9ceb178a949492b3990a858fe25
minxng <minxng12@gmail.com>
2025-03-12 00:25:51 +0900

    param 오류 수정

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -3,17 +3,19 @@ import MovieInfo, { getMovie } from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
 
 interface IParams {
-  params: { id: string };
+  id: string;
 }
 
-export async function generateMetadata({ params: { id } }: IParams) {
+export async function generateMetadata({ params }: { params: IParams }) {
+  const { id } = params;
   const movie = await getMovie(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetail({ params: { id } }: IParams) {
+export default async function MovieDetail({ params }: { params: IParams }) {
+  const { id } = params;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info..</h1>}>
